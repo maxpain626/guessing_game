a70053a4d008e600cda736c340abf0fc73902061
maxpain <greyhack01@rambler.ru>
2025-04-27 16:23:13 +0300

    use std::cmp::Ordering;  match guess.cmp(&secret_number)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 
 fn main() {
     println!("Угадайте число");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
     println!("Секретное число: {}", secret_number);
     println!("Введите догадку: ");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Не получилось прочитать строку");
     println!("Вы загадали: {}", guess);
+    let guess: u32 = guess.trim().parse().expect("Введите число.");
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Слишком малое число."),
+        Ordering::Greater => println!("Слишком большое число."),
+        Ordering::Equal => println!("Верное число!"),
+    }
 }
