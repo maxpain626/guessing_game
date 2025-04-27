c4be46c6713237dcec0b6b376b018875acc40f5e
maxpain <greyhack01@rambler.ru>
2025-04-27 15:31:19 +0300

    use rand::Rng; thread_rng().gen_range(1, 101);

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
+use rand::Rng;
 
 
 fn main() {
     println!("Угадайте число");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Секретное число: {}", secret_number);
     println!("Введите догадку: ");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Не получилось прочитать строку");
