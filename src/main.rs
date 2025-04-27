5e4f7822177d714eecf5d1810361ff093985f9f4
maxpain <greyhack01@rambler.ru>
2025-04-27 17:57:42 +0300

    attempts 2

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         let game_over: bool = false;
         let mut attempts: u32 = 0;
         while !game_over{
-            println!("Секретное число: {}", secret_number);
+            //println!("Секретное число: {}", secret_number);
             println!("Введите догадку: ");
             let mut guess = String::new();
             io::stdin().read_line(&mut guess).expect("Не получилось прочитать строку");
