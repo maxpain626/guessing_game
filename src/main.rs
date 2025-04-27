c3b29e1100804a31edc6643913c4ab9827a5b92a
maxpain <greyhack01@rambler.ru>
2025-04-27 17:56:19 +0300

    attempts

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
         let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
         println!("Новая игра! Угадайте число");
         let game_over: bool = false;
+        let mut attempts: u32 = 0;
         while !game_over{
             println!("Секретное число: {}", secret_number);
             println!("Введите догадку: ");
@@ -18,12 +19,13 @@ fn main() {
                 Err(_) => continue,
             };
             println!("Вы загадали: {}", guess);
-            
+            attempts += 1;
+            println!("{} попытка.", &attempts);
             match guess.cmp(&secret_number) {
                 Ordering::Less => println!("Слишком малое число.\n"),
                 Ordering::Greater => println!("Слишком большое число.\n"),
                 Ordering::Equal => {
-                    println!("Поздравляю! Верное число!\n");
+                    println!("\nПоздравляю! Верное число!\nЭто заняло у вас {} попыток", &attempts);
                     break;
                 }
             }
