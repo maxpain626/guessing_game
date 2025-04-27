0e466adadc89979aa1e24b022f6948f289658bd7
maxpain <greyhack01@rambler.ru>
2025-04-27 17:36:45 +0300

    loop{         let secret_number: u32 = rand::thread_rng().gen_range(1, 101);

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,27 @@ use std::cmp::Ordering;
 
 fn main() {
     loop{
-        println!("Угадайте число");
         let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
-        println!("Секретное число: {}", secret_number);
-        println!("Введите догадку: ");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Не получилось прочитать строку");
-        let guess: u32 = match guess.trim().parse(){
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("Вы загадали: {}", guess);
-        
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Слишком малое число.\n"),
-            Ordering::Greater => println!("Слишком большое число.\n"),
-            Ordering::Equal => {
-                println!("Верное число!\n");
-                break;
+        println!("Новая игра! Угадайте число");
+        let game_over: bool = false;
+        while !game_over{
+            println!("Секретное число: {}", secret_number);
+            println!("Введите догадку: ");
+            let mut guess = String::new();
+            io::stdin().read_line(&mut guess).expect("Не получилось прочитать строку");
+            let guess: u32 = match guess.trim().parse(){
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            println!("Вы загадали: {}", guess);
+            
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Слишком малое число.\n"),
+                Ordering::Greater => println!("Слишком большое число.\n"),
+                Ordering::Equal => {
+                    println!("Поздравляю! Верное число!\n");
+                    break;
+                }
             }
         }
     }
